c183a749f8d62509a86922f275c51d0fb1320b4b
Eddie Belter <ebelter@gmail.com>
2015-04-22 14:40:56 -0500

    Welcome page

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | UserAuth"
+  end
+end
