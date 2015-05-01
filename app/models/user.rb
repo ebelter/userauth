6781e436724595f8f42549bd7dece5ef62918987
Eddie Belter <ebelter@gmail.com>
2015-04-30 23:58:33 -0500

    User: rm attr_accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
 
-  attr_accessor :name, :email
-
   validates :name, presence: true, length: { :maximum => 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
