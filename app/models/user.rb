28e3d8cbada9a83cec9dbcbf051e494dbb235eb4
Eddie Belter <ebelter@gmail.com>
2015-05-06 10:26:46 -0500

    Add user activation resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { self.email = email.downcase }
+  before_create :create_activation_digest
+  before_save   :downcase_email
 
   validates :name, presence: true, length: { :maximum => 50 }
 
@@ -41,5 +42,18 @@ class User < ActiveRecord::Base
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
