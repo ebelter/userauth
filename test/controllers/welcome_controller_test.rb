08e2e2b69aeb3c9b65b4bae8cca538490f8b3313
Eddie Belter <ebelter@gmail.com>
2015-11-25 20:35:56 -0600

    Use base_title method

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -5,18 +5,18 @@ class WelcomeControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | UserAuth"
+    assert_select "title", "Home | " + base_title
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | UserAuth"
+    assert_select "title", "About | " + base_title
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | UserAuth"
+    assert_select "title", "Contact | " + base_title
   end
 end
