799a69330dc026903c985c8f973354ade85e6ed6
Eddie Belter <ebelter@gmail.com>
2015-11-28 21:47:33 -0600

    Use app name helper instead of UserAuth

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title, "UserAuth"
-    assert_equal full_title("Home"), "Home | UserAuth"
+    assert_equal full_title, app_name, 'title helper'
+    assert_equal full_title("Home"), "Home | " + app_name, 'title helper for Home'
   end
 end
